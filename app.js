0b97e1926c39b347c3fe656998dab565ccb000df
Denis Sharikadze <denissharikadze94@yandex.ru>
2023-06-14 22:45:01 +0400

    feat: add routers

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 
+const router = require('./routes/index');
+
 const { PORT, DB_PATH } = process.env;
 const { PORT_DEV, DB_PATH_DEV } = require('./utils/constants');
 const { isProduction } = require('./utils/utils');
