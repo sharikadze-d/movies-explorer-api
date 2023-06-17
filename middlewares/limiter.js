b3152cbdeea0d811b166a9d5e64dd72eb7191577
Denis Sharikadze <denissharikadze94@yandex.ru>
2023-06-17 23:59:25 +0400

    fix: limiter

--- middlewares/limiter.js
+++ middlewares/limiter.js
@@ -2,7 +2,7 @@ const rateLimiter = require('express-rate-limit');
 const { ERROR_LIMITER } = require('../utils/constants');
 
 const limiter = rateLimiter({
-  max: 5,
+  max: 30,
   windowMS: 10000, // 10 seconds
   message: ERROR_LIMITER,
 });
